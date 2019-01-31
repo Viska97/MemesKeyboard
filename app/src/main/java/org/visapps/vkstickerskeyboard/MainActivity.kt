a3985708447b4d13f887dcbb0a4e40c80f939969
VISApps <m.viska97@gmail.com>
2019-01-31 18:34:19 +0300

    upd

--- app/src/main/java/org/visapps/vkstickerskeyboard/MainActivity.kt
+++ app/src/main/java/org/visapps/vkstickerskeyboard/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
 import com.vk.sdk.VKSdk
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
+            R.id.navigation_allstickers -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
+            R.id.navigation_savedstickers -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
+            R.id.navigation_account -> {
                 return@OnNavigationItemSelectedListener true
             }
         }
